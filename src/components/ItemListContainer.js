bb35988b49198f8f7031c1451c2a4b5c06c8c695
mariaflorenciaferreira <florferreirag@gmail.com>
2022-03-05 16:25:05 -0300

    useState useEffect

--- src/components/ItemListContainer.js
+++ src/components/ItemListContainer.js
@@ -1,13 +1,21 @@
 import ItemCount from "./ItemCount"
+import ItemList from "./ItemList";
+import {useState,useEffect} from "react"
+
+
+
 
 
 function ItemListContainer(props){
-    return <main className="ItemListContainer">
-        <h2 >Bienvenido {props.nombre} {props.apellido}!  </h2>
-        <ItemCount />
-        
 
-    </main>
+        return( 
+        <main className="ItemListContainer">
+            <h2 >Bienvenido {props.nombre} {props.apellido}!  </h2>
+            <ItemCount />
+            <ItemList />
+
+        </main>
+    );
 
 }
 
