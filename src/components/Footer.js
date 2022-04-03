5c477d6446cf1690e6b990e0de1170359d07feb3
mariaflorenciaferreira <florferreirag@gmail.com>
2022-04-03 16:43:21 -0300

    footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,26 @@
 import LogoIcon from "./LogoIcon"
+import {Link} from "react-router-dom"
+import {FaInstagram,FaFacebook,FaMapMarkedAlt} from "react-icons/fa";
+
 
 function Footer(){
-    return (
-       <div className="footer"> 
-            <LogoIcon/>
-            <h2>VIVERO</h2>
-        </div>
-      
-        
+    return (          
+            <div className="footer">
+                <div className="footer1">
+                    <LogoIcon/>
+                     <Link to="/" className="footerBrandLink" >VIVERO</Link>
+                </div>
+                
+              
+
+                <div className="footer2">                
+                    <p className="footerIcon"><FaInstagram/></p>
+                    <p className="footerIcon"><FaFacebook/></p>
+                    <p className="footerIcon"><FaMapMarkedAlt/></p>
+                </div>
+                 
+            </div>
+   
     )
 }
 
